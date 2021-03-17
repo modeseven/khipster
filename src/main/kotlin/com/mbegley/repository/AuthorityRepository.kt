6d5f82ae581ec0bdb6db2e12a835fd2a292a107f
mbegley <mikeabegley@gmail.com>
2021-03-17 18:29:26 -0400

    Initial version of khipster generated by JHipster-6.10.5 with blueprints: kotlin-1.11.0

---
+++ src/main/kotlin/com/mbegley/repository/AuthorityRepository.kt
@@ -0,0 +1,10 @@
+package com.mbegley.repository
+
+import com.mbegley.domain.Authority
+import org.springframework.data.jpa.repository.JpaRepository
+
+/**
+ * Spring Data JPA repository for the [Authority] entity.
+ */
+
+interface AuthorityRepository : JpaRepository<Authority, String>
---
+++ src/test/kotlin/com/mbegley/config/NoOpMailConfiguration.kt
@@ -0,0 +1,21 @@
+package com.mbegley.config
+
+import com.mbegley.domain.User
+import com.mbegley.service.MailService
+import org.mockito.Mockito
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class NoOpMailConfiguration {
+    private val mockMailService = Mockito.mock(MailService::class.java)
+
+    @Bean
+    fun mailService(): MailService {
+        return mockMailService
+    }
+
+    init {
+        Mockito.doNothing().`when`(mockMailService).sendActivationEmail(User())
+    }
+}
